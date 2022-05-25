f9bb0a5868c0b2a5ad4d60038887b7df73687179
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-05-25 13:03:28 +0500

    education section

--- src/Collect.jsx
+++ src/Collect.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Components/HomePage/HomePage';
+import Education from './Components/Education/Education';
 
 const Collect = () => {
   return (
     <div className='container-fluid'>
       <Navbar/>
       <HomePage/>
+      <Education/>
     </div>
   );
 };
