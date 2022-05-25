fbd86cef0609276ea9355c6c2bd3b8d29b508bd1
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-05-25 13:00:27 +0500

    home carusel

--- src/Collect.jsx
+++ src/Collect.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Components/HomePage/HomePage';
 
 const Collect = () => {
   return (
     <div className='container-fluid'>
+      <Navbar/>
       <HomePage/>
     </div>
   );
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,20 +1,91 @@
 import React from 'react';
+import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div>
-      <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Centered hero</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-          </div>
-        </div>
-      </div>
+    <div className='HomePage'>
+      <div className="px-4 py-5 my-5 text-center">
+<h1 className="display-5 fw-bold">Centered hero</h1>
+<div className="col-lg-6 mx-auto">
+  <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap,  extensive prebuilt components, and powerful JavaScript plugins.</p>
+  <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    <button type="button" className="btn btn-success btn-lg px-4 gap-3">Primary button</button>
+  </div>
+</div>
+</div> 
     </div>
   );
 };
 
 export default HomePage;
+
+
+{/* <div className="px-4 py-5 my-5 text-center">
+<h1 className="display-5 fw-bold">Centered hero</h1>
+<div className="col-lg-6 mx-auto">
+  <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap,  extensive prebuilt components, and powerful JavaScript plugins.</p>
+  <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    <button type="button" className="btn btn-success btn-lg px-4 gap-3">Primary button</button>
+  </div>
+</div>
+</div>   */}
+
+
+
+// <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+
+// <div class="carousel-indicators">
+//   <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+//   <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
+//   <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+// </div>
+
+// <div class="carousel-inner">
+
+//   <div class="carousel-item">
+//     <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+//     <div class="container">
+//       <div class="carousel-caption text-start">
+//         <div className="px-4 py-5 my-5 text-center">
+//           <h1 className="display-5 fw-bold">Centered hero</h1>
+
+//           <div className="col-lg-6 mx-auto">
+//             <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap,  extensive prebuilt components, and powerful JavaScript plugins.</p>
+//             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+//               <button type="button" className="btn btn-success btn-lg px-4 gap-3">Primary button</button>
+//             </div>
+//           </div>
+//         </div> 
+//       </div>
+//     </div>
+//   </div>
+
+//   <div class="carousel-item">
+//     <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+//     <div class="container">
+//       <div class="carousel-caption">
+//         <h1>Another example headline.</h1>
+//         <p>Some representative placeholder content for the second slide of the carousel.</p>
+//         <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+//       </div>
+//     </div>
+//   </div>
+
+//   <div class="carousel-item active">
+//     <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+//     <div class="container">
+//       <div class="carousel-caption text-end">
+//         <h1>One more for good measure.</h1>
+//         <p>Some representative placeholder content for the third slide of this carousel.</p>
+//         <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+//       </div>
+//     </div>
+//   </div>
+
+// </div>
+
+
+// </div>
