dfbd6821403c83e994a31415a538f4159c0625c4
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-05-27 20:10:43 +0500

    carusel ishladi

--- src/Components/Caruser/Caruser.jsx
+++ src/Components/Caruser/Caruser.jsx
@@ -11,38 +11,45 @@ import img1 from "../img/woliul-hasan-atNNDyP-3hE-unsplash.jpg"
 const Caruser = ({img}) => {
   return (
     <div className="Caruser">
-          <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
-          <FontAwesomeIcon icon={faAngry}></FontAwesomeIcon>
-          <img src={img}/>
-          <img src={img1} />
-
-       <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={img1} className="d-block w-100" alt="..."/>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={img1} class="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
           </div>
-          <div className="carousel-item">
-            <img src={img1} className="d-block w-100" alt="..."/>
+          <div class="carousel-item">
+            <img src={img1} class="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
           </div>
-          <div className="carousel-item">
-            <img src={img1} className="d-block w-100" alt="..."/>
+          <div class="carousel-item">
+            <img src={img1} class="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
           </div>
         </div>
-
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">          
-          </span>
+          <span class="visually-hidden">Previous</span>
         </button>
-
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
         </button>
-
       </div>
     </div>
-  
   );
 };
 
